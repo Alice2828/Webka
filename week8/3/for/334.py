027a7da58275ff0992f3dcf34668648199ba683e
Alice2828 <alisalebedeva28@mail.ru>
2020-03-18 08:47:18 +0600

    334,335 changed

--- week8/3/for/334.py
+++ week8/3/for/334.py
@@ -2,7 +2,7 @@ a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
-
-for i in range(a, b):
+      
+for i in range (a, b+1):
     if i % d == c:
-        print(i)
+        print(i, end=' ')
--- week8/3/for/335.py
+++ week8/3/for/335.py
@@ -3,6 +3,11 @@ import math
 a = int(input())
 b = int(input())
 
-for i in range(a, b):
-    if math.modf(math.sqrt(i))[0] == 0:
-        print(i)
+# for i in range(a, b):
+#     if math.modf(math.sqrt(i))[0] == 0:
+#         print(i)
+
+
+for i in range (a, b+1):
+    if math.sqrt(i) == math.ceil(math.sqrt(i)):
+        print(i, end=' ')
