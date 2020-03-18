4381330219bcf230a9ce25239de6bcc5f851e422
Alice2828 <alisalebedeva28@mail.ru>
2020-03-18 08:42:31 +0600

    333 done

--- week8/3/for/333.py
+++ week8/3/for/333.py
@@ -1,7 +1,15 @@
 a = int(input())
 b = int(input())
-for i in range(a, b+1,2):
-    if(i % 2 == 1):
-        i+=1
-    print(i, end =" ")
+# for i in range(a, b+1,2):
+#     if(i % 2 == 1):
+#         i+=1
+#     print(i, end =" ")
 
+
+if a == b:
+    if a % 2 == 0:
+        print(a)
+else:
+    for i in range(a, b+1):
+        if i % 2 == 0:
+            print(i, end=' ')
