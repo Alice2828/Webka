8fb4d3cf30dd38a0fa6aef48851cae62c44319a6
Alice2828 <alisalebedeva28@mail.ru>
2020-03-19 16:22:26 +0600

    Minus power

--- week8/5/307.py
+++ week8/5/307.py
@@ -1,7 +1,9 @@
 def power(a, n):
     if n == 0:
         return 1
-    return a*power(a, n-1)
+    if n>0:
+        return a*power(a, n-1)
+    return (1/a)*(1/power(1/a,n+1))
 
 
 arr = [int(i) for i in input().split()]
