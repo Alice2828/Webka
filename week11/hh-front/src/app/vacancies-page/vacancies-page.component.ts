fd82ccbbefed8aee5fcea349dd4bd040fc049a6e
Alice2828 <47051363+Alice2828@users.noreply.github.com>
2020-04-16 11:19:15 +0600

    vacancies

--- week11/hh-front/src/app/vacancies-page/vacancies-page.component.ts
+++ week11/hh-front/src/app/vacancies-page/vacancies-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import{VacancyService} from '../vacancy.service'
+import { CompanyService } from "../company.service";
+import { Company, Vacancy } from "../../models"
 
 @Component({
   selector: 'app-vacancies-page',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vacancies-page.component.css']
 })
 export class VacanciesPageComponent implements OnInit {
-
-  constructor() { }
+  vacancy:Vacancy
+  constructor(private companyService: CompanyService,private vacancyService: VacancyService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getVacancy();
+  }
+  getVacancy() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.vacancyService.getVacancy(id).subscribe(vacancy => this.vacancy = vacancy);
   }
 
 }
